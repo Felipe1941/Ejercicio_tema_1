3aef9beb9f710a610f499797df9397b64be5fb9c
Felipe1941 <leer.be@hotmail.com>
2022-12-05 12:51:15 -0500

    Initial commit

---
+++ src/Paquete4/Creacion_de_clase.java
@@ -0,0 +1,31 @@
+package Paquete4;
+
+public class Creacion_de_clase {
+    public static void main(String[] args) {
+        //numericos
+        byte variable1 = 1;
+        System.out.println(variable1);
+        short variable2 = 10;
+        System.out.println(variable2);
+        int variable3= 100;
+        System.out.println(variable3);
+        long variable4 = 1000;
+        System.out.println(variable4);
+        float variable5 = 1.1f;
+        System.out.println(variable5);
+        double variable6= 5.25;
+        System.out.println(variable6);
+
+        //booleano
+        boolean variable7= true;
+        System.out.println(variable7);
+        boolean variable8 = false;
+        System.out.println(variable8);
+
+        //texto
+        char variable9 = 'a';
+        System.out.println(variable9);
+        String variable10 = "Hola mundo";
+        System.out.println(variable10);
+    }
+}
